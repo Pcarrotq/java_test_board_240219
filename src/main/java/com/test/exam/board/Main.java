f5f58915111275956fb9c60240e0ee29a48580fd
Pcarrotq <bags9638@gmail.com>
2024-02-23 17:05:35 +0900

    수정

--- src/main/java/com/test/exam/board/Main.java
+++ src/main/java/com/test/exam/board/Main.java
@@ -1,57 +1,56 @@
 package com.test.exam.board;
+
 import java.util.*;
 
 public class Main {
   static void makeTestData(List<Article> articles) {
     articles.add(new Article(1, "제목1", "내용1"));
-    articles.add(new Article(1, "제목2", "내용2"));
-    articles.add(new Article(1, "제목3", "내용3"));
+    articles.add(new Article(2, "제목2", "내용2"));
+    articles.add(new Article(3, "제목3", "내용3"));
   }
-
   public static void main(String[] args) {
     System.out.println("== 자바 텍스트 게시판 0.1v ==");
     System.out.println("== 자바 텍스트 게시판 시작 ==");
-
     Scanner sc = new Scanner(System.in);
-    Article lastArticle = null;
     int articleLastId = 0;
-
-    ArrayList<Article> articles = new ArrayList<>();
+    List<Article> articles = new ArrayList<>();
     makeTestData(articles);
-
-    if (articles.size() > 0) {
+    if(articles.size() > 0) {
       articleLastId = articles.get(articles.size() - 1).id;
     }
-
     while (true) {
       System.out.printf("명령) ");
       String cmd = sc.nextLine();
-
       Rq rq = new Rq(cmd);
       Map<String, String> params = rq.getParams();
-
-      if (rq.getUrlPath().equals("/user/article/write")) {
+      if (rq.getUrlPath().equals("/usr/article/write")) {
         System.out.println("== 게시물 작성 ==");
         System.out.printf("제목 : ");
         String title = sc.nextLine();
         System.out.printf("내용 : ");
         String body = sc.nextLine();
-
-        int id = ++articleLastId; // articleLastId + 1
-
+        int id = ++articleLastId;
         Article article = new Article(id, title, body);
-        lastArticle = article;
-
         articles.add(article);
-        System.out.printf("%d번 게시물이 생성되었습니다.\n", id);
+        System.out.printf("%d번 게시물이 생성되었습니다.\n", article.id);
       }
-      else if (rq.getUrlPath().equals("/user/article/detail")) {
-        if (articles.isEmpty()) {
+      else if (rq.getUrlPath().equals("/usr/article/list")) {
+        System.out.println("== 게시물 리스트 ==");
+        System.out.println("===================");
+        System.out.println("번호 / 제목");
+        System.out.println("===================");
+        for(int i = articles.size() - 1; i >= 0; i--) {
+          Article article = articles.get(i);
+          System.out.printf("%d / %s\n", article.id, article.title);
+        }
+      }
+      else if (rq.getUrlPath().equals("/usr/article/detail")) {
+        if(articles.isEmpty()) {
           System.out.println("게시물이 존재하지 않습니다.");
           continue;
         }
 
-        if (params.containsKey("id") == false) {
+        if(params.containsKey("id") == false) {
           System.out.println("id를 입력해주세요.");
           continue;
         }
@@ -61,39 +60,25 @@ public class Main {
         try {
           id = Integer.parseInt(params.get("id"));
         } catch (NumberFormatException e) {
-          System.out.println("id를 정수 형태로 입력해주세요.");
+          System.out.println("id를 정수형태로 입력해주세요.");
           continue;
         }
 
-        if (id > articles.size()) {
+        if(id > articles.size()) {
           System.out.printf("%d번 게시물이 존재하지 않습니다.\n", id);
           continue;
         }
-
-        Article article = articles.get(articles.size() - 1);
-
-        System.out.println("== 게시물 상세 내용 ==");
-        System.out.printf("id : %d\n", article.id);
-        System.out.printf("title : %s\n", article.title);
-        System.out.printf("body : %s\n", article.body);
-      }
-      else if (rq.getUrlPath().equals("/user/article/list")) {
-        System.out.println("== 게시물 리스트 ==");
-        System.out.println("===================");
-        System.out.println("번호 / 제목");
-        System.out.println("===================");
-
-        for (int i = articles.size() - 1; i >= 0; i--) {
-          Article article = articles.get(i);
-          System.out.printf("%d / %s\n", article.id, article.title);
-        }
+        Article article = articles.get(id - 1);
+        System.out.println("== 게시물 상세내용 ==");
+        System.out.printf("번호 : %d\n", article.id);
+        System.out.printf("제목 : %s\n", article.title);
+        System.out.printf("내용 : %s\n", article.body);
       }
       else if (rq.getUrlPath().equals("exit")) {
         System.out.println("프로그램을 종료합니다.");
         break;
       }
     }
-
     System.out.println("== 자바 텍스트 게시판 종료 ==");
     sc.close();
   }
